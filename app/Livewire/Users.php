16e17666b248ae6fcecdc8a32f0e86713dbdcece
feliksarydwisaputra <felixarydwisaputra@gmail.com>
2025-10-25 18:05:52 +0800

    reapir title users

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -2,6 +2,14 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
-class Users extends Component {}
+class Users extends Component
+{
+    #[Title('User Page')]
+    public function render()
+    {
+        return view('livewire.users');
+    }
+}
